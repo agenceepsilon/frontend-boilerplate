cf4d9e88e97ac153ec7ced42adcfac647d7241f7
Boris Damevin <bdamevin@tequilarapido.com>
2014-05-16 13:48:14 +0200

    Cleaning HTML/CSS forms

--- components/modules/sample-forms.php
+++ components/modules/sample-forms.php
@@ -5,48 +5,46 @@
 
 <div class="mod mod--form-sample">
     <form class="form" action="#" method="post">
-        <div class="form-row">
+        <div class="form_row">
             <label>Inputs / Textarea</label>
         </div>
-        <div class="form-row">
-            <div class="form-input">
+        <div class="form_row">
+            <div class="form_input">
                 <input type="text" placeholder="Type Text">
             </div>
         </div>
-        <div class="form-row">
-            <div class="form-input">
+        <div class="form_row">
+            <div class="form_input">
                 <input type="password" value="Type Password">
             </div>
         </div>
-        <div class="form-row">
+        <div class="form_row">
             <textarea placeholder="Textarea"></textarea>
         </div>
-        <div class="form-row">
+        <div class="form_row">
             <label>Buttons</label>
         </div>
-        <div class="form-row">
-            <button class="btn">Balise Button</button>
+        <div class="form_row">
+            <div class="btn">
+                <input type="submit" value="Type Submit">
+            </div>
         </div>
-        <div class="form-row">
-            <input class="btn" type="button" value="Type Button">
+        <div class="form_row">
+            <div class="btn">
+                <input type="submit" disabled="disabled" value="Type Submit - Option Disabled">
+            </div>
         </div>
-        <div class="form-row">
-            <input class="btn" type="submit" value="Type Submit">
-        </div>
-        <div class="form-row">
-            <input class="btn" type="submit" disabled="disabled" value="Type Submit - Option Disabled">
-        </div>
-        <div class="form-row">
+        <div class="form_row">
             <div class="btn"><a href="#">Button HTML "a"</a></div>
         </div>
-        <div class="form-row">
+        <div class="form_row">
             <div class="btn"><span>Button HTML "span"</span></div>
         </div>
-        <div class="form-row">
+        <div class="form_row">
             <label>Selects</label>
         </div>
-        <div class="form-row">
-            <div class="form-select js-form-select">
+        <div class="form_row">
+            <div class="form_select js-form-select">
                 <select>
                     <option>Option 1</option>
                     <option>Option 2</option>
@@ -54,19 +52,19 @@
                 </select>
             </div>
         </div>
-        <div class="form-row">
+        <div class="form_row">
             <label>Checkbox</label>
         </div>
-        <div class="form-row">
-            <div class="form-checkbox">
+        <div class="form_row">
+            <div class="form_checkbox">
                 <input type="checkbox" id="checkbox"> <label for="checkbox">Checkbox</label>
             </div>
         </div>
-        <div class="form-row">
+        <div class="form_row">
             <label>Radio</label>
         </div>
-        <div class="form-row">
-            <div class="form-radio">
+        <div class="form_row">
+            <div class="form_radio">
                 <input type="radio" id="radio"> <label for="radio">Radio</label>
             </div>
         </div>
