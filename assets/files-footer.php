1a585665509055ccdbeaa32028c99354c4e0cb2f
Boris Damevin <bdamevin@tequilarapido.com>
2014-06-20 17:55:53 +0200

    Fix Hx bold

--- assets/files-footer.php
+++ assets/files-footer.php
@@ -6,6 +6,6 @@
 <script src="assets/js/main.js"></script>
 
 <!-- Grunt Browser Sync - Intégration uniquement -->
-<script src='//localhost:3000/socket.io/socket.io.js'></script>
-<script>var ___socket___ = io.connect('http://localhost:3000');</script>
-<script src='//localhost:3001/client/browser-sync-client.0.9.1.js'></script>
+<!--<script src='//localhost:3000/socket.io/socket.io.js'></script>-->
+<!--<script>var ___socket___ = io.connect('http://localhost:3000');</script>-->
+<!--<script src='//localhost:3001/client/browser-sync-client.0.9.1.js'></script>-->
