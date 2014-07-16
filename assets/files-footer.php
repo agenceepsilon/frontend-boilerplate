87c888352523366f87866718c5f1997652e9f81e
Boris Damevin <bdamevin@agenceepsilon.com>
2014-07-17 01:44:51 +0200

    [#2] Grunt : Browser Sync 1.3.0

--- assets/files-footer.php
+++ assets/files-footer.php
@@ -6,6 +6,8 @@
 <script src="assets/js/main.js"></script>
 
 <!-- Grunt Browser Sync - NOT for the production -->
-<script src='//localhost:3000/socket.io/socket.io.js'></script>
-<script>var ___socket___ = io.connect('http://localhost:3000');</script>
-<script src='//localhost:3001/client/browser-sync-client.0.9.1.js'></script>
+<script type='text/javascript'>
+//<![CDATA[
+    document.write("<script async src='//HOST:3000/browser-sync-client.1.3.0.js'><\/script>".replace(/HOST/g, location.hostname));
+//]]>
+</script>
