3dd2a886bc688f0f502c795e59dd17ecf1abedf1
Boris Damevin <bdamevin@agenceepsilon.com>
2014-10-25 22:44:01 +0200

    Grunt: css min banner

--- grunt-tasks/cssmin.js
+++ grunt-tasks/cssmin.js
@@ -1,4 +1,7 @@
 module.exports = {
+    options: {
+        banner: '/*! <%= pkg.name %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    },
     css: {
         files: {
             "<%= assets.css %>/dist/style.min.css": "<%= assets.css %>/style.css",
