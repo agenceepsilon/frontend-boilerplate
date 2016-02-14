612d17f3b5ac169f42558ab0536bc301c62eff67
Boris Damevin <borisdamevin@gmail.com>
2016-02-14 12:29:00 -0500

    Applied fixes from StyleCI

--- components/modules/_sample-typography.php
+++ components/modules/_sample-typography.php
@@ -25,20 +25,20 @@
 
     <h2>Paragraphs</h2>
 
-    <p><?php echo lipsum(80, 100, "words", false); ?></p>
+    <p><?php echo lipsum(80, 100, 'words', false); ?></p>
 
-    <p><?php echo lipsum(70, 90, "words", false); ?></p>
+    <p><?php echo lipsum(70, 90, 'words', false); ?></p>
 
     <p>
         <img class="alignleft" alt="" src="http://placehold.it/100x100" height="100" width="100">
-        <?php echo lipsum(70, 90, "words", false); ?>
+        <?php echo lipsum(70, 90, 'words', false); ?>
     </p>
 
-    <p><?php echo lipsum(100, 110, "words", false); ?></p>
+    <p><?php echo lipsum(100, 110, 'words', false); ?></p>
 
     <p>
         <img class="alignright" alt="" src="http://placehold.it/100x100" height="100" width="100">
-        <?php echo lipsum(100, 120, "words", false); ?>
+        <?php echo lipsum(100, 120, 'words', false); ?>
     </p>
 
     <p>
@@ -90,7 +90,7 @@
     <h2>Blockquote</h2>
 
     <blockquote>
-        <p><?php echo lipsum(45, 65, "words", false); ?></p>
+        <p><?php echo lipsum(45, 65, 'words', false); ?></p>
     </blockquote>
 
     <h2>Table</h2>
