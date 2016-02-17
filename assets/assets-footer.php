0a9be7ba38f053afcd17bf54e6c68f80a1f26383
Boris Damevin <bdamevin@agenceepsilon.com>
2016-02-17 23:22:56 +0100

    jQuery 2.x.x. by default

--- assets/assets-footer.php
+++ assets/assets-footer.php
@@ -1,7 +1,7 @@
 <?php if (ENVIRONMENT == 'local'): ?>
 
     <!-- JS libraries files -->
-    <script src="<?php echo NPM_VENDOR ?>/jquery/dist/jquery.js?v=<?php echo ASSETS_VERSION ?>"></script>
+    <script src="<?php echo NPM_VENDOR ?>/jquery/dist/jquery.min.js?v=<?php echo ASSETS_VERSION ?>"></script>
 
     <!-- JS main file -->
     <script src="<?php echo ASSETS_JAVASCRIPT ?>/main.js?v=<?php echo ASSETS_VERSION ?>"></script>
