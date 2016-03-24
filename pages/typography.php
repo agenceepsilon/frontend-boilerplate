0f7cca35753875c799ceb9c7e3bb067c10909900
Boris Damevin <borisdamevin@gmail.com>
2016-03-24 13:10:22 -0400

    Applied fixes from StyleCI

--- pages/typography.php
+++ pages/typography.php
@@ -5,8 +5,8 @@
     /*
      * Display modules
      */
-    $modules = array (
-        'main' => array (
-            '1' => '_sample-typography'
-        )
-    );
+    $modules =  [
+        'main' =>  [
+            '1' => '_sample-typography',
+        ],
+    ];
